50cecbdca2abb1851b707a989dc7743f6ce2c73d
rafR <rrojano@gmail.com>
2013-03-21 11:21:14 -0600

    arranque de sysgen

---
+++ src/main/java/mx/uv/sysgen/App.java
@@ -0,0 +1,13 @@
+package mx.uv.sysgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
