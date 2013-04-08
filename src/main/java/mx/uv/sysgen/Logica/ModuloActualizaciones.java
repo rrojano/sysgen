e51ae7817a56aebdeb7ce3d815a8b81f6c089fc9
Santiu <Santiu@Santiu-PC.lan>
2013-04-07 19:15:05 -0500

    -Inclusión de el módulo logico de Actualizaciones de catálogo -Diagramas de Actualizaciones de catálogo -Interfaz de Actualizaciones de catálogo

---
+++ src/main/java/mx/uv/sysgen/Logica/ModuloActualizaciones.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.uv.sysgen.Logica;
+
+import mx.uv.sysgen.Actualizaciones;
+
+
+/**
+ *
+ * @author Santiu
+ */
+public class ModuloActualizaciones{
+    Actualizaciones ventana = new Actualizaciones();    
+}
