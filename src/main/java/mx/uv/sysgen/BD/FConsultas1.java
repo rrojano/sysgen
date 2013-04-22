968f2188516011d74cce6310c3def5aa8736c9ae
Luis Enrique <Luis Enrique@172.72.53.51>
2013-04-22 09:10:15 -0500

    consultas asistidas

--- src/main/java/mx/uv/sysgen/BD/FConsultas1.java
+++ src/main/java/mx/uv/sysgen/BD/FConsultas1.java
@@ -4,13 +4,23 @@
  */ 
 package mx.uv.sysgen.BD;
 
+import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -22,12 +32,63 @@ public class FConsultas1 extends javax.swing.JFrame {
     String u;
     String c;
     String e;
+    String tabla;
+    LinkedList<String> letreros=new LinkedList<String>();
+    LinkedList<JTextField> campos=new LinkedList<JTextField>();
 
-public void tomaDatos(DefaultTableModel modelo){
-    if (this.jTable2 == null)
-        this.jTable2 = new JTable();
-        this.jTable2.setModel(modelo);
-/*     */   }
+public LinkedList<String> meteCampos(String tabla) throws SQLException{
+     BD base=new BD();
+     this.configuraBD(base);
+     ResultSet rs=base.consulta("select * from "+tabla);
+     rs.next();
+     System.out.println("select * from "+tabla);
+        
+     try{
+           ResultSetMetaData metaDatos = rs.getMetaData();
+           int numeroColumnas = metaDatos.getColumnCount();
+           LinkedList<String> etiquetas = new LinkedList<String> ();
+           for (int i = 0; i < numeroColumnas; i++){
+               etiquetas.add(metaDatos.getColumnLabel(i + 1));
+               System.out.println(metaDatos.getColumnLabel(i + 1));
+           }
+           return etiquetas;
+     }catch (Exception e) {
+           e.printStackTrace();
+           return null;
+     }
+
+}    
+    
+public LinkedList <String> meteTablas(){
+     BD base=new BD();
+     configuraBD(base);
+     LinkedList<String> arr= new LinkedList<String>();
+    try {
+      DatabaseMetaData md = base.conexion.getMetaData();
+      ResultSet rs = md.getTables(null, null, "%", null);
+      
+      while (rs.next()) {
+            arr.add((rs.getString(3)));       
+      }
+      return arr;
+    } catch (SQLException ex) {
+        Logger.getLogger(FConsultas1.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    return null;
+ }
+    
+public void tomaDatos(DefaultTableModel modelo,int i){
+    if (i==1){
+    if (this.JTable == null)
+        this.JTable = new JTable();
+        this.JTable.setModel(modelo);
+    }
+    else{
+        if (this.JTable2 == null)
+        this.JTable2 = new JTable();
+        this.JTable2.setModel(modelo);
+    }
+}
 
 public void configuraBD(BD b){
     b.login=u;
@@ -67,30 +128,37 @@ public void configuraBD(BD b){
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox1 = new javax.swing.JComboBox();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
+        JTable = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        JCBtabla = new javax.swing.JComboBox();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        PNcampos = new javax.swing.JPanel();
+        BTNbuscar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        JTable2 = new javax.swing.JTable();
+        BTNxml2 = new javax.swing.JButton();
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel());
-        jScrollPane1.setViewportView(jTable2);
+        JTable.setModel(new javax.swing.table.DefaultTableModel());
+        jScrollPane1.setViewportView(JTable);
 
         jLabel1.setText("Consulta");
 
         jTextField1.setToolTipText("Escriba la consulta que será realizada por el manejador de base de datos preseleccionado");
         jTextField1.setName("TFconsulta"); // NOI18N
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
 
         jLabel2.setText("Manejador: ");
         jLabel2.setName("LBLmanejador"); // NOI18N
@@ -148,6 +216,106 @@ public void configuraBD(BD b){
 
         jTabbedPane1.addTab("Consulta SQL", jPanel1);
 
+        jPanel2.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent evt) {
+                jPanel2ComponentShown(evt);
+            }
+        });
+
+        JCBtabla.setModel(new javax.swing.DefaultComboBoxModel());
+        JCBtabla.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JCBtablaActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Manejador: ");
+        jLabel3.setName("LBLmanejador"); // NOI18N
+
+        jLabel4.setText("Tabla");
+
+        PNcampos.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        javax.swing.GroupLayout PNcamposLayout = new javax.swing.GroupLayout(PNcampos);
+        PNcampos.setLayout(PNcamposLayout);
+        PNcamposLayout.setHorizontalGroup(
+            PNcamposLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 149, Short.MAX_VALUE)
+        );
+        PNcamposLayout.setVerticalGroup(
+            PNcamposLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 172, Short.MAX_VALUE)
+        );
+
+        BTNbuscar.setText("Buscar");
+        BTNbuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNbuscarActionPerformed(evt);
+            }
+        });
+
+        JTable2.setModel(new javax.swing.table.DefaultTableModel());
+        jScrollPane2.setViewportView(JTable2);
+
+        BTNxml2.setText("XML");
+        BTNxml2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNxml2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 254, Short.MAX_VALUE)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(PNcampos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(26, 26, 26)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(JCBtabla, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(30, 30, 30)
+                                .addComponent(BTNbuscar)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BTNxml2)
+                        .addGap(24, 24, 24))))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addComponent(PNcampos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(BTNxml2)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JCBtabla, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BTNbuscar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3))
+        );
+
+        jTabbedPane1.addTab("Consulta Apoyada", jPanel2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -168,35 +336,106 @@ public void configuraBD(BD b){
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+        if (resultados!=null){
+            ResultSetToXML xml=new ResultSetToXML();
+            try{
+                xml.crearXML(resultados);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         DefaultTableModel modelo = new DefaultTableModel();
         BD base=new BD();
         configuraBD(base);
         ResultSet rs = base.consulta(this.jTextField1.getText());
-        
+
         if (rs!=null){
-        resultados= base.consulta(this.jTextField1.getText());
-        ConversorResultSetADefaultTableModel.rellena(rs, modelo);
-        tomaDatos(modelo);
+            resultados= base.consulta(this.jTextField1.getText());
+            ConversorResultSetADefaultTableModel.rellena(rs, modelo);
+            tomaDatos(modelo,1);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jPanel2ComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel2ComponentShown
+       LinkedList<String> a=this.meteTablas();
+        for (int i=0;i<a.size();i++){
+          JCBtabla.addItem(a.get(i));
+        }// TODO add your handling code here:        // TODO add your handling code here:
+    }//GEN-LAST:event_jPanel2ComponentShown
+
+    private void JCBtablaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JCBtablaActionPerformed
+        String tab=(String) JCBtabla.getSelectedItem();
+        System.out.println(tab);
+        tabla=tab;
+        letreros=new LinkedList <String>();
+        campos=new LinkedList <JTextField>();
+        try {
+            letreros=this.meteCampos(tab);
+        } catch (SQLException ex) {
+            Logger.getLogger(FConsultas1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        PNcampos.removeAll();
+        for (int i=0;i<letreros.size();i++){
+            this.PNcampos.add(new JLabel(letreros.get(i))); 
+            campos.add(new JTextField());
+            this.PNcampos.add(campos.get(i));
+        }
+        PNcampos.setLayout(new GridLayout(campos.size(), 2, 0, 0));
+        PNcampos.updateUI();    
+        //new JLabel(campos.get(i);
+    }//GEN-LAST:event_JCBtablaActionPerformed
+
+    private void BTNbuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNbuscarActionPerformed
+        String cons="select * from "+tabla;
+        boolean ban=false;
+        boolean ini=false;
+        int cont=0;
+        String extra="where ";
+        for (int i=0;i<letreros.size();i++){
+           if (campos.get(i).getText()!="" && !campos.get(i).getText().isEmpty()){
+               ban=true;  
+               if (ban==true && cont>0){
+                   extra=extra+" and ";
+                   }
+               extra=" "+extra + letreros.get(i)+"="+campos.get(i).getText();
+               cont=cont+1;
+           }
+        }
+        if (ban!=false)
+            cons=cons+extra;
+        System.out.println(cons);
+        //-------------------------------------------------------------------
+        DefaultTableModel modelo = new DefaultTableModel();
+        BD base=new BD();
+        configuraBD(base);
+        ResultSet rs = base.consulta(cons);
+
+        if (rs!=null){
+            resultados= base.consulta(cons);
+            ConversorResultSetADefaultTableModel.rellena(rs, modelo);
+            tomaDatos(modelo,2);
+        }
+        
+    }//GEN-LAST:event_BTNbuscarActionPerformed
+
+    private void BTNxml2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNxml2ActionPerformed
         // TODO add your handling code here:
         if (resultados!=null){
-          ResultSetToXML xml=new ResultSetToXML();
-          try{
-          xml.crearXML(resultados);          
-          }
-          catch(Exception e){
-              e.printStackTrace();
-          }
+            ResultSetToXML xml=new ResultSetToXML();
+            try{
+                xml.crearXML(resultados);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
         }
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_BTNxml2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -229,18 +468,29 @@ public void configuraBD(BD b){
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new FConsultas1().setVisible(true);
+                
             }
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BTNbuscar;
+    private javax.swing.JButton BTNxml2;
+    private javax.swing.JComboBox JCBtabla;
+    private javax.swing.JTable JTable;
+    private javax.swing.JTable JTable2;
+    private javax.swing.JPanel PNcampos;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
