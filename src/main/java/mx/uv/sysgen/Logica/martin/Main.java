6c6124440870bab50913ff71793f975b8f82b37f
GON <GON@APOLO.lan>
2013-05-14 10:06:55 -0500

    Codigo para agregar plantillas v4.1

--- src/main/java/mx/uv/sysgen/Logica/martin/Main.java
+++ src/main/java/mx/uv/sysgen/Logica/martin/Main.java
@@ -1,13 +1,21 @@
 package mx.uv.sysgen.Logica.martin;
 
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author GON
+ */
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         interfazAgregar inte = new interfazAgregar();
+         VPlantilla inte = new VPlantilla();
          inte.setVisible(true);
     }
 }
